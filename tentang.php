f6ea79d7c2f06fa4b1811fd2ce290a56d743a658
aniknovita <39272713+aniknovita@users.noreply.github.com>
2018-05-22 20:53:57 +0700

    novita

---
+++ tentang.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Tekno Shop</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <?php include 'navigation.php' ?>
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading/Breadcrumbs -->
+      <h1 class="mt-4 mb-3">tentang
+        <small>Tekno Shop</small>
+      </h1>
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">Tentang</li>
+      </ol>
+
+      <!-- Intro Content -->
+      <div class="row">
+        <div class="col-lg-6">
+          <img class="img-fluid rounded mb-4" src="../foto_produk/back1.png" alt="">
+        </div>
+        <div class="col-lg-6">
+          <h2>Tentang Tekno Shop</h2>
+          <p align="justify">
+            Tekno Shop menjual berbagai macam kategori produk laptop, gadget dan komputer.Mulai dari Ultrabook, PC desktop, PC tablet, Smartphone, Gadget, Kamera digital, Camcorder, Printer, Projector, Scanner, Software, hingga berbagai macam peripheral dan aksesoris baik untuk kamera, tablet, laptop maupun PC desktop.
+          </p>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <!-- Team Members -->
+      <h2>Our Team</h2>
+
+      <div class="row">
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100 text-center">
+            <img class="card-img-top" src="../foto_produk/winarto.jpg" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Winarto</h4>
+              <h6 class="card-subtitle mb-2 text-muted">Position</h6>
+              <p class="card-text">
+                CEO Tekno Shop
+              </p>
+            </div>
+            <div class="card-footer">
+              <a href="#">sinarto@teknoshop.com</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100 text-center">
+            <img class="card-img-top" src="../foto_produk/winarto.jpg" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Novita</h4>
+              <h6 class="card-subtitle mb-2 text-muted">Position</h6>
+              <p class="card-text">
+                CEO Tekno Shop
+              </p>
+            </div>
+            <div class="card-footer">
+              <a href="#">novita@teknoshop.com</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100 text-center">
+            <img class="card-img-top" src="../foto_produk/winarto.jpg" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Ayu</h4>
+              <h6 class="card-subtitle mb-2 text-muted">Position</h6>
+              <p class="card-text">
+                CEO Tekno Shop
+              </p>
+            </div>
+            <div class="card-footer">
+              <a href="#">ayu@teknoshop.com</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+ <?php include 'footer.php' ?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
